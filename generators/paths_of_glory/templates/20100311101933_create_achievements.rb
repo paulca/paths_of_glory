4ca70ea5927d19f85c3d2d57cab19f163241b2de
Michael Kohl <citizen428@gmail.com>
2010-06-22 21:06:31 +0800

    Fixed database migration

--- generators/paths_of_glory/templates/20100311101933_create_achievements.rb
+++ generators/paths_of_glory/templates/20100311101933_create_achievements.rb
@@ -1,10 +1,10 @@
 class CreateAchievements < ActiveRecord::Migration
   def self.up
     create_table :achievements do |t|
-      t.string :type
+      t.string  :type
       t.integer :level
       t.integer :user_id
-      t.notified :boolean, :default => false
+      t.boolean :notified, :default => false
 
       t.timestamps
     end
